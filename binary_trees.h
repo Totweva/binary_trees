05c1c3f06d6b6b7a98eb6657d4ee7c97cc40d066
Totweva <totweva@hmail.com>
2022-11-17 08:25:24 -0800

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define _BINARY_TREES_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -33,3 +34,4 @@ typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+#endif /* binary trees */
